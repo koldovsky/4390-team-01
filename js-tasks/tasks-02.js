6c8682ddb1fc9d5ae9778e738a181bf32f7435bd
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2025-07-21 21:51:23 +0300

    Update tasks-02.js - mykola dron code

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -76,3 +76,14 @@ function getRealFloor(n) {
     return n-2;
   }
 }
+
+//mykola dron
+// не зовсім зрозумів як це робити - але ось завдання з codewars
+//1
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+//2
+function stringToArray(str) {
+  return str.split(' ');
+}
