089b8b46067fe735d700305d90b7b0d13813c548
DBondar <163669859+OneBlueFrog@users.noreply.github.com>
2025-07-22 19:37:49 +0000

    Edit

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -7,25 +7,22 @@ function stringToArray(string) {
 //Завдання 1 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 //DeinekaAndrii
 function stringToArray(string) {
-   return (string.match(/[а-яёa-zієїґ0-9’']+/gi) || []);
+  return (string.match(/[а-яёa-zієїґ0-9’']+/gi) || []);
 }
 
 //Завдання 2 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
   result = "";
-  
-  for(let i = 0; i < dna.length; i++)
-    {
-      if(dna[i] === "T")
-        {
-          result += "U";
-        }
-      else
-        {
-          result += dna[i];
-        }
+
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "T") {
+      result += "U";
     }
-  
+    else {
+      result += dna[i];
+    }
+  }
+
   return result;
 }
 function DNAtoRNA(dna) {
@@ -54,14 +51,14 @@ var max = function (list) {
 //Поглиблені задачі
 //Завдання 3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 //DeinekaAndrii
-var min = function(list){
-    const minValue = Math.min(...list);
-    return minValue;
+var min = function (list) {
+  const minValue = Math.min(...list);
+  return minValue;
 }
 
-var max = function(list){
-    const maxValue = Math.max(...list);
-    return maxValue;
+var max = function (list) {
+  const maxValue = Math.max(...list);
+  return maxValue;
 }
 
 //Завдання 4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
@@ -95,11 +92,11 @@ function min(arr, toReturn) {
 
 //Завдання 4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 //DeinekaAndrii
-function min(arr, toReturn) { 
+function min(arr, toReturn) {
   const minValue = Math.min(...arr);
   const indexOfMinValue = arr.indexOf(minValue);
-  if(toReturn === "value") return minValue;
-  if(toReturn === "index") return indexOfMinValue;
+  if (toReturn === "value") return minValue;
+  if (toReturn === "index") return indexOfMinValue;
 }
 
 //Додаткові завдання
@@ -112,81 +109,82 @@ function doubleInteger(i) {
 //End code Fedir
 
 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
+function stringToArray(string) {
 
-  let words=string.split(" ");
+  let words = string.split(" ");
   return words;
-	// code code code
+  // code code code
 
 }
 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
- return dna.replaceAll("T","U"); 
-  
+  return dna.replaceAll("T", "U");
+
   // create a function which returns an RNA sequence from the given DNA sequence
 }
 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-var min = function(list){
-  list.sort((a,b)=>a-b);
-    return list[0];
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
 }
-var max = function(list){
-  list.sort((a,b)=>b-a);
-    return list[0];
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
 }
 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-function min(arr, toReturn) { 
-  let min =Math.min(...arr);
-  if(toReturn==="value"){
+function min(arr, toReturn) {
+  let min = Math.min(...arr);
+  if (toReturn === "value") {
     return min;
-  }else if(toReturn==="index"){
+  } else if (toReturn === "index") {
     return arr.indexOf(min);
   }
-    
+
 }
 https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 function doubleInteger(i) {
   // i will be an integer. Double it and return it.
-  return i*2;
+  return i * 2;
 }
 https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-  const age = Math.abs(dadYearsOld- (sonYearsOld * 2));
-console.log(age);
+  const age = Math.abs(dadYearsOld - (sonYearsOld * 2));
+  console.log(age);
   return age;
   // your code here
-  
+
 }
 https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
-function nthEven(n){
-return (n-1)*2;
+function nthEven(n) {
+  return (n - 1) * 2;
 
 }
 https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
-function past(h, m, s){
-  
-  return (h*3600*1000)+(m*60*1000)+(s*1000);
+function past(h, m, s) {
+
+  return (h * 3600 * 1000) + (m * 60 * 1000) + (s * 1000);
   //#Happy Coding! ^_^
 }
 https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 function isDivisible(n, x, y) {
-if(n%x===0 && n%y===0){
-  return true;}
-  else{
+  if (n % x === 0 && n % y === 0) {
+    return true;
+  }
+  else {
     return false;
   }
 }
 https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
 function getRealFloor(n) {
- if(n<0){
-   return n;
- }else if(n===1||n===0){
-   return 0;
- }
-  else if(n>=2&&n<=12){
-    return n-1;
-  }else if(n>=13){
-    return n-2;
+  if (n < 0) {
+    return n;
+  } else if (n === 1 || n === 0) {
+    return 0;
+  }
+  else if (n >= 2 && n <= 12) {
+    return n - 1;
+  } else if (n >= 13) {
+    return n - 2;
   }
 }
 
@@ -201,3 +199,38 @@ function stringToArray(str) {
   return str.split(' ');
 }
 
+//Denys Bondar
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+  return dna.replace("T", "U");
+}
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function (list) {
+  return Math.min(...list);
+}
+
+var max = function (list) {
+  return Math.max(...list);
+}
+console.log(min([1, 2, 3]));
+console.log(max([1, 2, 3]));
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
+  const minValue = Math.min(...arr);
+  if (toReturn === "value") {
+    return minValue;
+  } else if (toReturn === "index") {
+    return arr.indexOf(minValue);
+  } else {
+    throw new Error("Second parameter must be 'value' or 'index'");
+  }
+}
+console.log(min([1, 2, 3, 4, 5], "value"));
+console.log(min([1, 2, 3, 4, 5], "index")); 
