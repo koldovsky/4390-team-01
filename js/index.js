953432ab374318b817e66290c9f14b4f4cd6c033
Yehor <egor.gonzha@gmail.com>
2025-07-26 12:04:08 +0000

    Fixed an issue with importing js file (removed from <head>)

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
-  let loadedPartialsCount = 0;
-
-  function init() {
+function init() {
     import('./index.tariffs.js');
     import('./index.available-cars.js');
-  }
+}
 
-  document.body.addEventListener('html:afterOnLoad', () => {
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
     loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) {
-      init();
-    }
-  });
+    if (loadedPartialsCount === totalPartials) init();
 });
