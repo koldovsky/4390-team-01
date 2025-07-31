6bd75d3dd5183a7f8001ce775c979910838de87a
Andriy0206 <les.andriy75@gmail.com>
2025-07-31 22:32:56 +0300

    my partial

--- js/index.tariffs.js
+++ js/index.tariffs.js
@@ -1,20 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const groupLinks = document.querySelectorAll(".tariffs__group-link");
-  const groups = document.querySelectorAll(".tariffs__group");
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const now = new Date();
 
-  groupLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
 
-      const targetId = link.getAttribute("href").substring(1);
+  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
 
-      groups.forEach((group) => {
-        if (group.id === targetId) {
-          group.classList.remove("tariffs__group--hidden");
-        } else {
-          group.classList.add("tariffs__group--hidden");
-        }
-      });
+setInterval(updateClock, 1000);
+updateClock(); 
+
+
+
+const tariffsTitle = document.querySelector('.tariffs__title');
+tariffsTitle.style.color = 'blue';
+
+const groupLinks = document.querySelectorAll('.tariffs__group-link');
+const groups = document.querySelectorAll('.tariffs__group');
+
+groupLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute('href').substring(1);
+
+    groups.forEach(group => {
+      if (group.id === targetId) {
+        group.classList.remove('tariffs__group--hidden');
+      } else {
+        group.classList.add('tariffs__group--hidden');
+      }
     });
   });
 });
